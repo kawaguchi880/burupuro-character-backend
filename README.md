c779baf7b14c5767e2531496e9e38d1c4946a96b
kawaguchi880 <yaoshengchuankou1@gmail.com>
2023-04-14 00:26:10 +0900

    requirements.txtの追加など

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # burupuro-character
+
+仮想環境に入る
+.\venv\Scripts\Activate.ps1
+
+requirements.txt に出力
+pip freeze > requirements.txt
+
+requirements.txt をもとに install
+pip install -r requirements.txt
--- apps/test1app/app.py
+++ apps/test1app/app.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello,Flaskbook!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+@app.route("/hello")
+def tekitou():
+    return "こんにちは！川口遥生さん！！"
