31eb8e6f2307d80a963194cc94bc8b29530f5d50
kawaguchi880 <yaoshengchuankou1@gmail.com>
2023-04-20 01:08:11 +0900

    アップロードされたファイルのサイズ制限や上書き、悪意のあるファイルなどの防止

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 CORS(app)
 app.config["UPLOAD_FOLDER"] = os.path.join(os.getcwd(), "images")
+app.config["MAX_CONTENT_LENGTH"] = 1 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = {"jpeg"}
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1] in ALLOWED_EXTENSIONS
 
 
 @app.route("/")
@@ -22,10 +29,12 @@ if __name__ == "__main__":
 @app.route("/api/upload", methods=["POST"])
 def upload_file():
     file = request.files["file"]
-    filename = secure_filename(file.filename)
-    random_hex = secrets.token_hex(8)
-    _, ext = os.path.splitext(filename)
-    new_filename = random_hex + ext
-    print(app.config["UPLOAD_FOLDER"])
-    file.save(os.path.join(app.config["UPLOAD_FOLDER"], new_filename))
-    return jsonify({"message": "File uploaded successfully."}), 200
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        random_hex = secrets.token_hex(8)
+        _, ext = os.path.splitext(filename)
+        new_filename = random_hex + ext
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], new_filename))
+        return jsonify({"message": "File uploaded successfully."}), 200
+    else:
+        return jsonify({"message": "invalid file."}), 200
