704d1d220c0d78953acd470f99d88a74b5d1f4f4
kawaguchi880 <yaoshengchuankou1@gmail.com>
2023-04-19 23:20:17 +0900

    フロントから送られてきた画像ファイルをimagesフォルダに保存まで

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+import os
+import secrets
+
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 CORS(app)
+app.config["UPLOAD_FOLDER"] = os.path.join(os.getcwd(), "images")
 
 
 @app.route("/")
@@ -14,6 +19,13 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 
-@app.route("/hello")
-def tekitou():
-    return "こんにちは！川口遥生さん！！"
+@app.route("/api/upload", methods=["POST"])
+def upload_file():
+    file = request.files["file"]
+    filename = secure_filename(file.filename)
+    random_hex = secrets.token_hex(8)
+    _, ext = os.path.splitext(filename)
+    new_filename = random_hex + ext
+    print(app.config["UPLOAD_FOLDER"])
+    file.save(os.path.join(app.config["UPLOAD_FOLDER"], new_filename))
+    return jsonify({"message": "File uploaded successfully."}), 200
